d268629145efd778657b25da0c8b819059a31965
Red-Raven <gecko723@yahoo.com>
2013-06-07 16:27:29 -0300

    Update World.java

--- World.java
+++ World.java
@@ -1,7 +1,7 @@
 package game;
 
 public class World {
-  
+	
 	private Place[][] myPlaceGrid;
 	private int myLength;
 	private int myWidth;
@@ -16,6 +16,22 @@ public class World {
 	public Place getPlace(int x, int y)
 	{
 		return myPlaceGrid[x][y];
+	}
+	
+	public Place getPlaceInDirection(Place currentPlace, String direction)
+	{
+		int x = currentPlace.getX();
+		int y = currentPlace.getY();
+		
+		if (direction.equals("East") && x + 1 < myLength)
+			return myPlaceGrid[x + 1][y];
+		else if (direction.equals("South") && y + 1 < myWidth)
+			return myPlaceGrid[x][y + 1];
+		else if (direction.equals("West") && x - 1 > 0 )
+			return myPlaceGrid[x - 1][y];
+		else if (direction.equals("North") && y - 1 > 0)
+			return myPlaceGrid[x][y - 1];
+		return null;
 	}
 
 }
