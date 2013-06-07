3da24f8ad75e8ff3206c677ed1e338d43ac5317b
Red-Raven <gecko723@yahoo.com>
2013-06-07 15:28:07 -0400

    Update Place.java

--- Place.java
+++ Place.java
@@ -1,8 +1,12 @@
+package game;
+
 public class Place {
 
-  private String myName;
+	private String myName;
 	private int myLength;
 	private int myWidth;
+	private int myXPosition;
+	private int myYPosition;
 	private int myEastEntranceX;
 	private int myEastEntranceY;
 	private int mySouthEntranceX;
@@ -13,12 +17,14 @@ public class Place {
 	private int myNorthEntranceY;
 	private Object[][] myGrid;
 	
-	public Place(String name, int length, int width, int eastEntranceX, int eastEntranceY, int southEntranceX, int southEntranceY, int westEntranceX, int westEntranceY, int northEntranceX, int northEntranceY)
+	public Place(String name, int length, int width, int xPosition, int yPosition, int eastEntranceX, int eastEntranceY, int southEntranceX, int southEntranceY, int westEntranceX, int westEntranceY, int northEntranceX, int northEntranceY)
 	{
 		myName = name;
 		myLength = length;
 		myWidth = width;
 		myGrid = new Object[myLength][myWidth];
+		myXPosition = xPosition;
+		myYPosition = yPosition;
 		myEastEntranceX = eastEntranceX;
 		myEastEntranceY = eastEntranceY;
 		mySouthEntranceX = southEntranceX;
@@ -39,5 +45,15 @@ public class Place {
 		return myName;
 	}
 	//add descriptions
+
+	public int getX()
+	{
+		return myXPosition;
+	}
 	
+	public int getY()
+	{
+		return myYPosition;
+	}
 }
+
