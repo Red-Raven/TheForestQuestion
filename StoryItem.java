71219962b4076d93737c908fe7e29592d3b4d070
Red-Raven <gecko723@yahoo.com>
2013-06-07 15:41:15 -0300

    Create StoryItem.java

---
+++ StoryItem.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.ArrayList;
+
+public class StoryItem {
+
+  private ArrayList<StoryObject> myUsableObjects;
+
+	//Give this a few general uses
+	//Possibly add some other types of specific StoryItem extendables
+	//extendable? What does that mean?
+	//allow others o create new storyItem types
+}
